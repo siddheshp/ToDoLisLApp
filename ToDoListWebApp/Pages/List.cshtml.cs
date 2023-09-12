03ae0661264b895f075c9b4c9a54ebf424b5dfb3
Siddhesh Prabhugaonkar <siddheshpg@gmail.com>
2023-09-12 13:03:44 +0530

    Add project files.

---
+++ ToDoListWebApp/Pages/List.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoListWebApp.Models;
+using ToDoListWebApp.Services;
+
+namespace ToDoListWebApp.Pages
+{
+    public class ListModel : PageModel
+    {
+        private readonly ToDoService toDoService;
+        public List<ToDo> ToDos { get; set; }
+        public ListModel(ToDoService toDoService)
+        {
+            this.toDoService = toDoService;
+        }
+        public async Task OnGet()
+        {
+            ToDos = await toDoService.GetToDosAsync();
+            return;
+        }
+    }
+}
---
+++ ToDoListWebApp/Services/ToDoService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListWebApp.Models;
+
+namespace ToDoListWebApp.Services
+{
+    public class ToDoService
+    {
+        private readonly ToDoContext context;
+
+        public ToDoService(ToDoContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<ToDo>> GetToDosAsync()
+        {
+            return await context.ToDos.ToListAsync();
+        }
+        public async Task AddAsync(ToDo toDo)
+        {
+            await context.ToDos.AddAsync(toDo);
+            await context.SaveChangesAsync();
+        }
+    }
+}
